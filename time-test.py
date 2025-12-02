54430e20c43a41f926d746bef52c6da4d995c632
Vanago <vananovoselov824@gmail.com>
2025-12-02 19:49:46 +0200

    Переробив скрипт виміру часу

--- time-test.py
+++ time-test.py
@@ -1,15 +1,41 @@
-import timeit
+import time
+import statistics
 
-# для коректного запуску тесту timeit
-setup_code = """
-import random
-from __main__ import graph_generator
-"""
+# тут потрібно імпортувати graph_generator та graph_algorythm
 
-time = timeit.timeit(
-    stmt="graph_generator(100, 50)",  # код який тестується
-    setup=setup_code,
-    number=50  # кількість ітерацій
-)
 
-print(time)
+# початкові значення для тесту
+vertices = 10 # вершини (20-200)
+density = 50 # щільність (10-50)
+num_iter = 0 # кількість ітерацій для тесту
+
+times = [] # список для результатів
+
+# тест алгоритму
+for i in range(num_iter):
+    #graph = graph_generator(vertices, density)
+
+    start_time = time.perf_counter() # початок заміру
+
+    # тут виклик функції алгоритму
+
+    end_time = time.perf_counter() # кінець заміру
+
+    clean_time = end_time - start_time
+    times.append(clean_time)
+
+
+if times:
+    avg_time = statistics.mean(times)
+
+    # Стандартне відхилення - показує, наскільки сильно відрізнялися результати між собою (дисперсія)
+    stdev = statistics.stdev(times)
+
+    print("-" * 50)
+    print(f"Результати тесту на {num_iter} ітерацій:")
+    print(f"Середній час {avg_time * 1000:.6f} мс")
+    print(f"Максимальний час: {max(times) * 1000:.6f} мс")
+    print(f"Мінімальний час: {min(times) * 1000:.6f} мс")
+    print(f"Стандартне відхилення: {stdev * 1000:.6f} мс")
+else:
+    print("Не вдалося зібрати дані вимірювань.")
