1dae550c03d2d463f0bbd28d1fccf5cc47d9d987
Vanago <vananovoselov824@gmail.com>
2025-12-02 15:53:42 +0200

    Зробив скрипт генерації графа

---
+++ graph-generation.py
@@ -0,0 +1,21 @@
+import random
+
+
+def graph_generator(vertices: int, density: int) -> list:
+    """
+    vertices - вершини графа,
+    density - щільність графа (0-100),
+    """
+
+    # створення пустого графа
+    matrix = [[0] * vertices for _ in range(vertices)]
+
+    # заповнення графа
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            if i == j:
+                matrix[i][j] = 1
+
+            if random.randint(1, 100) <= density:
+                matrix[i][j] = 1
+    return matrix
